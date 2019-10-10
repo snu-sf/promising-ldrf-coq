9092860ce899f161d63a24ecb6dae32d7d2b5419
Sung-HwanLee <hwan.lee67@gmail.com>
2019-10-11 02:09:46 +0900

    Fix fetch-add operation

--- src/while/Semantics.v
+++ src/while/Semantics.v
@@ -33,7 +33,7 @@ Module RegFile.
   Definition eval_rmw (rf:t) (rmw:Instr.rmw) (val:Const.t): Const.t * option Const.t :=
     match rmw with
     | Instr.fetch_add addendum =>
-      (Const.add val (eval_value rf addendum), Some (Const.add val (eval_value rf addendum)))
+      (val, Some (Const.add val (eval_value rf addendum)))
     | Instr.cas o n =>
       if Const.eq_dec (eval_value rf o) val
       then (1, Some (eval_value rf n))
