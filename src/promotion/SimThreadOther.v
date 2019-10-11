45703d6aa69d46c8d83166e197fd13b531f88497
Sung-HwanLee <hwan.lee67@gmail.com>
2019-10-11 23:30:25 +0900

    Refactor

--- src/promotion/SimThreadOther.v
+++ src/promotion/SimThreadOther.v
@@ -85,6 +85,44 @@ Module SimThreadOther.
       erewrite promise_eq_mem; try exact PROMISE; eauto.
   Qed.
 
+  Lemma sim_thread_program_step
+        l e1_src
+        e_tgt e1_tgt e2_tgt
+        (SIM1: sim_thread l e1_src e1_tgt)
+        (WF1_SRC: Local.wf e1_src.(Thread.local) e1_src.(Thread.memory))
+        (WF1_TGT: Local.wf e1_tgt.(Thread.local) e1_tgt.(Thread.memory))
+        (SC1_SRC: Memory.closed_timemap e1_src.(Thread.sc) e1_src.(Thread.memory))
+        (SC1_TGT: Memory.closed_timemap e1_tgt.(Thread.sc) e1_tgt.(Thread.memory))
+        (CLOSED1_SRC: Memory.closed e1_src.(Thread.memory))
+        (CLOSED1_TGT: Memory.closed e1_tgt.(Thread.memory))
+        (STEP_TGT: Thread.program_step e_tgt e1_tgt e2_tgt):
+    exists e_src e2_src,
+      <<STEP_SRC: Thread.opt_program_step e_src e1_src e2_src>> /\
+      <<EVENT: ThreadEvent.get_machine_event e_src = ThreadEvent.get_machine_event e_tgt>> /\
+      <<SIM2: sim_thread l e2_src e2_tgt>> /\
+      <<MEMLOC: forall to, Memory.get l to e1_src.(Thread.memory) = Memory.get l to e2_src.(Thread.memory)>>.
+  Proof.
+    inv STEP_TGT; ss.
+    hexploit loc_free_step_is_accessing_loc; eauto; i.
+    { inv SIM1. ss. subst. eauto. }
+    exploit program_step; try exact LOCAL; try eapply SIM1; eauto. i. des.
+    destruct e1_src. ss.
+    esplits.
+    - econs 2. econs; try exact STEP_SRC.
+      inv SIM1. inv STATE0. ss.
+      rewrite H0. rewrite EVENT2. eauto.
+    - ss.
+    - econs; eauto.
+      + s. eapply step_loc_free; eauto.
+        inv SIM1. ss. subst. ss.
+      + s. i. inv SIM1.
+        erewrite <- program_step_eq_promises; eauto.
+        erewrite ThreadEvent.eq_program_event_eq_loc; eauto.
+    - s. i.
+      eapply program_step_eq_mem; eauto.
+      erewrite ThreadEvent.eq_program_event_eq_loc; eauto.
+  Qed.
+
   Lemma sim_thread_step
         l e1_src
         pf e_tgt e1_tgt e2_tgt
@@ -103,36 +141,22 @@ Module SimThreadOther.
       <<MEMLOC: forall to, Memory.get l to e1_src.(Thread.memory) = Memory.get l to e2_src.(Thread.memory)>>.
   Proof.
     inv STEP_TGT.
-    { (* promise step *)
-      exploit sim_thread_promise_step; eauto;
-        try apply WF1_SRC; try apply WF1_TGT.
-      i. des.
-      esplits; [|M| |]; eauto.
-      - inv STEP_SRC.
-        + econs 1.
-        + econs 2. econs 1; eauto.
-      - inv STEP. ss. inv STEP_SRC; ss. inv STEP. ss.
-    }
-    (* program step *)
-    inv STEP; ss.
-    hexploit loc_free_step_is_accessing_loc; eauto; i.
-    { inv SIM1. ss. subst. eauto. }
-    exploit program_step; try exact LOCAL; try eapply SIM1; eauto. i. des.
-    destruct e1_src. ss.
-    esplits.
-    - econs 2. econs 2. econs; try exact STEP_SRC.
-      inv SIM1. inv STATE0. ss.
-      rewrite H0. rewrite EVENT2. eauto.
-    - ss.
-    - econs; eauto.
-      + s. eapply step_loc_free; eauto.
-        inv SIM1. ss. subst. ss.
-      + s. i. inv SIM1.
-        erewrite <- program_step_eq_promises; eauto.
-        erewrite ThreadEvent.eq_program_event_eq_loc; eauto.
-    - s. i.
-      eapply program_step_eq_mem; eauto.
-      erewrite ThreadEvent.eq_program_event_eq_loc; eauto.
+    - exploit sim_thread_promise_step; eauto. i. des.
+      esplits.
+      + inv STEP_SRC.
+        * econs 1.
+        * econs 2. econs 1; eauto.
+      + inv STEP. ss. inv STEP_SRC; ss. inv STEP. ss.
+      + ss.
+      + ss.
+    - exploit sim_thread_program_step; eauto. i. des.
+      esplits.
+      + inv STEP_SRC.
+        * econs 1.
+        * econs 2. econs 2; eauto.
+      + ss.
+      + ss.
+      + ss.
   Qed.
 
   Lemma sim_thread_opt_step
