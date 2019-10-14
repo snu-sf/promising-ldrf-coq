ac83a6dded34d71f7c4d0ed2cf7b4e997683a2d3
Sung-HwanLee <hwan.lee67@gmail.com>
2019-10-14 18:44:39 +0900

    WIP

--- src/opt/ReorderAbort.v
+++ src/opt/ReorderAbort.v
@@ -39,11 +39,12 @@ Inductive reorder_abort: forall (i2:Instr.t), Prop :=
     l2 v2 o2
     (ORD: Ordering.le o2 Ordering.acqrel):
     reorder_abort (Instr.store l2 v2 o2)
-| reorder_abort_update
-    r2 l2 rmw2 or2 ow2
-    (ORDR2: Ordering.le or2 Ordering.relaxed)
-    (ORDW2: Ordering.le ow2 Ordering.acqrel):
-    reorder_abort (Instr.update r2 l2 rmw2 or2 ow2)
+(* update should be handeled as global *)
+(* | reorder_abort_update *)
+(*     r2 l2 rmw2 or2 ow2 *)
+(*     (ORDR2: Ordering.le or2 Ordering.relaxed) *)
+(*     (ORDW2: Ordering.le ow2 Ordering.acqrel): *)
+(*     reorder_abort (Instr.update r2 l2 rmw2 or2 ow2) *)
 | reorder_abort_fence
     or2 ow2
     (ORDR2: Ordering.le or2 Ordering.relaxed)
@@ -98,32 +99,32 @@ Proof.
   inv SIM1. econs; eauto.
 Qed.
 
-Lemma sim_abort_cap
-      st_src lc_src sc1_src mem1_src
-      st_tgt lc_tgt sc1_tgt mem1_tgt
-      mem2_src
-      (MEM1: sim_memory mem1_src mem1_tgt)
-      (SIM1: sim_abort st_src lc_src sc1_src mem1_src
-                       st_tgt lc_tgt sc1_tgt mem1_tgt)
-      (CAP_SRC: Memory.cap lc_src.(Local.promises) mem1_src mem2_src):
-  exists lc'_src mem2_tgt,
-    <<MEM2: sim_memory mem2_src mem2_tgt>> /\
-    <<CAP_TGT: Memory.cap lc_tgt.(Local.promises) mem1_tgt mem2_tgt>> /\
-    <<SIM2: sim_abort st_src lc'_src sc1_src mem2_src
-                      st_tgt lc_tgt sc1_tgt mem2_tgt>>.
-Proof.
-  inv SIM1.
-  exploit Memory.cap_future_weak; try exact CAP_SRC; eauto. i.
-  exploit SimPromises.cap; try apply MEM1; eauto.
-  { inv LOCAL. apply SimPromises.sem_bot_inv in PROMISES; auto. rewrite <- PROMISES.
-    apply SimPromises.sem_bot.
-  }
-  i. des.
-  exploit cap_property; try exact CAP_SRC; eauto. i. des.
-  exploit cap_property; try exact CAP_TGT; eauto. i. des.
-  esplits; eauto.
-  econs; eauto.
-Qed.
+(* Lemma sim_abort_cap *)
+(*       st_src lc_src sc1_src mem1_src *)
+(*       st_tgt lc_tgt sc1_tgt mem1_tgt *)
+(*       mem2_src *)
+(*       (MEM1: sim_memory mem1_src mem1_tgt) *)
+(*       (SIM1: sim_abort st_src lc_src sc1_src mem1_src *)
+(*                        st_tgt lc_tgt sc1_tgt mem1_tgt) *)
+(*       (CAP_SRC: Memory.cap lc_src.(Local.promises) mem1_src mem2_src): *)
+(*   exists lc'_src mem2_tgt, *)
+(*     <<MEM2: sim_memory mem2_src mem2_tgt>> /\ *)
+(*     <<CAP_TGT: Memory.cap lc_tgt.(Local.promises) mem1_tgt mem2_tgt>> /\ *)
+(*     <<SIM2: sim_abort st_src lc'_src sc1_src mem2_src *)
+(*                       st_tgt lc_tgt sc1_tgt mem2_tgt>>. *)
+(* Proof. *)
+(*   inv SIM1. *)
+(*   exploit Memory.cap_future_weak; try exact CAP_SRC; eauto. i. *)
+(*   exploit SimPromises.cap; try apply MEM1; eauto. *)
+(*   { inv LOCAL. apply SimPromises.sem_bot_inv in PROMISES; auto. rewrite <- PROMISES. *)
+(*     apply SimPromises.sem_bot. *)
+(*   } *)
+(*   i. des. *)
+(*   exploit cap_property; try exact CAP_SRC; eauto. i. des. *)
+(*   exploit cap_property; try exact CAP_TGT; eauto. i. des. *)
+(*   esplits; eauto. *)
+(*   econs; eauto. *)
+(* Qed. *)
 
 Lemma sim_abort_steps_failure
       st1_src lc1_src sc1_src mem1_src
